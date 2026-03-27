5490f5a8dd258df6acda80e7802e6d7bd356c2a2
tainara.luza <tessdev3@gmail.com>
2026-03-27 09:51:35 +0000

    first commit

---
+++ csv-to-json/src/main/java/pt/luzagroup/csv_to_json/CsvToJsonApplication.java
@@ -0,0 +1,13 @@
+package pt.luzagroup.csv_to_json;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CsvToJsonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CsvToJsonApplication.class, args);
+	}
+
+}
---
+++ csv-to-json/src/test/java/pt/luzagroup/csv_to_json/CsvToJsonApplicationTests.java
@@ -0,0 +1,13 @@
+package pt.luzagroup.csv_to_json;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CsvToJsonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
